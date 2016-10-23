1b1475e9b8fb537578d903e62f94f17d83de8775
djangoman <djangoman8@gmail.com>
2016-10-24 00:42:16 +0530

    Djangoman 8 first commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/enceladus/PycharmProjects/Django_learning/web_machine/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
